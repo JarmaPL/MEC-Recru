1a6c506839496724b55fed5a7e76772522130e7f
JarmaPL <jarma.kanal@gmail.com>
2020-09-17 18:53:56 +0200

    first

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import { Div, Container } from "atomize";
+
+const Wrapper = styled.div`
+  width: 100%ś;
+  height: 100px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 15px;
+`;
+
+const BrandName = styled.div`
+  font-size: 42px;
+  font-weight: 600;
+  text-transform: uppercase;
+`;
+
+const NavigationBar = () => (
+  <Wrapper>
+    <BrandName>MEC.</BrandName>
+    <Div>
+      <p>Hello!</p>
+    </Div>
+  </Wrapper>
+);
+
+export default NavigationBar;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Provider as StyletronProvider } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
+
+const engine = new Styletron();
 
 ReactDOM.render(
-  <React.StrictMode>
+  <StyletronProvider value={engine}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StyletronProvider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
